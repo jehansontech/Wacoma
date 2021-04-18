9b4e5a193039ada5509366a659f0eb385b598789
Jim Hanson <jim@jehanson.org>
2021-04-18 12:25:10 -0400

    fix name

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,6 +1,6 @@
 import XCTest
 
-import UIStuffTests
+import UIStuffForSwiftTests
 
 var tests = [XCTestCaseEntry]()
 tests += UIStuffForSwiftTests.allTests()
--- Tests/UIStuffForSwiftTests/XCTestManifests.swift
+++ Tests/UIStuffForSwiftTests/XCTestManifests.swift
@@ -3,7 +3,7 @@ import XCTest
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(UIStuffTests.allTests),
+        testCase(UIStuffForSwiftTests.allTests),
     ]
 }
 #endif
