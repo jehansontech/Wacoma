afaa512e4448cbc3b198bdf8493b4cc0adea4c87
Jim Hanson <jim@jehanson.org>
2021-05-11 07:36:44 -0400

    float

---
+++ Sources/Taconic/Float+Extensions.swift
@@ -0,0 +1,40 @@
+//
+//  Float+Extensions.swift
+//  ArcWorld
+//
+//  Created by Jim Hanson on 10/13/20.
+//  Copyright © 2020 J.E. Hanson Technologies LLC. All rights reserved.
+//
+
+import Foundation
+ 
+extension Float {
+    
+    public static let twoPi: Float = 2 * .pi
+    
+    public static let piOverTwo: Float = 0.5 * .pi
+    
+    public static let threePiOverTwo: Float = 1.5 * .pi
+
+    public static let piOverFour: Float = 0.25 * .pi
+
+    public static let epsilon: Float = 1e-6
+    
+    public static let goldenRatio: Float = (0.5 * (1 + sqrt(5)))
+    
+    public static let logTwo: Float = log(2)
+    
+    public func clamp(_ min: Self, _ max: Self) -> Self {
+        return self < min ? min : (self > max ? max : self)
+    }
+
+    public func fuzz(_ fuzzFactor: Self) -> Self {
+        let lo = (1 - fuzzFactor) * self
+        let hi = (1 + fuzzFactor) * self
+        return Float.random(in: lo...hi)
+    }
+
+    public func differentFrom(_ x: Self) -> Bool {
+        return abs(x - self) > .epsilon
+    }
+ }
