2d077d0c0c6d6a6ba07d754a641459942b121bb5
Jim Hanson <jim@jehanson.org>
2022-06-27 14:24:57 -0400

    renaming

--- Sources/Wacoma/Numeric+Extensions.swift
+++ Sources/Wacoma/Numeric+Extensions.swift
@@ -9,12 +9,20 @@ import Foundation
 
 extension BinaryInteger {
 
+    public func clamp(_ range: ClosedRange<Self>) -> Self {
+        return (self < range.lowerBound) ? range.lowerBound : (self > range.upperBound ? range.upperBound : self)
+    }
+
     public func clamp(_ min: Self, _ max: Self) -> Self {
         return (self < min) ? min : (self > max ? max : self)
     }
 }
 
 extension BinaryFloatingPoint {
+
+    public func clamp(_ range: ClosedRange<Self>) -> Self {
+        return (self < range.lowerBound) ? range.lowerBound : (self > range.upperBound ? range.upperBound : self)
+    }
 
     public func clamp(_ min: Self, _ max: Self) -> Self {
         return (self < min) ? min : (self > max ? max : self)
